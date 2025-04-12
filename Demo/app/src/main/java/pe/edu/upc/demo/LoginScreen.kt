1b9dc1cde2caede974495f57f7f29192d3e6952d
jmayta1984 <jmayta@pucp.edu.pe>
2025-04-12 07:56:03 -0500

    feat: add initial Jetpack compose project with practical examples.

---
+++ Demo/app/src/main/java/pe/edu/upc/demo/LoginScreen.kt
@@ -0,0 +1,111 @@
+package pe.edu.upc.demo
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.filled.Visibility
+import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Preview
+@Composable
+fun LoginScreen() {
+    val username = remember {
+        mutableStateOf("")
+    }
+
+    val password = remember {
+        mutableStateOf("")
+    }
+
+    val isVisible = remember {
+        mutableStateOf(false)
+    }
+
+    Scaffold { innerPadding ->
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(innerPadding)
+        ) {
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                placeholder = {
+                    Text("Username")
+                },
+                value = username.value, onValueChange = {
+                    username.value = it
+                },
+                trailingIcon = {
+                    Icon(Icons.Default.Person, contentDescription = null)
+                }
+
+            )
+            OutlinedTextField(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                placeholder = {
+                    Text("Password")
+                },
+                visualTransformation =
+                    if (isVisible.value) VisualTransformation.None
+                    else PasswordVisualTransformation(),
+                value = password.value, onValueChange = {
+                    password.value = it
+                },
+                trailingIcon = {
+                    IconButton(
+                        onClick = {
+                            isVisible.value = !isVisible.value
+                        }
+                    ) {
+                        Icon(
+                            if (isVisible.value)
+                                Icons.Default.Visibility
+                            else
+                                Icons.Default.VisibilityOff,
+
+                            contentDescription = null
+                        )
+                    }
+                }
+            )
+
+            Button(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                onClick = {
+
+                }
+            ) {
+                Text("Login")
+            }
+        }
+
+    }
+}
+
